3e6645d94b537a7ba6126123ff9a341cabee140a
Tan <tan.tungo@gmail.com>
2026-02-19 15:15:59 +0700

    update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-copy /Y .\bin\Debug\net35\ring_of_love.dll "C:\Users\tantu\AppData\Roaming\r2modmanPlus-local\WizardOfLegend\profiles\my_mod\BepInEx\plugins"
+copy /Y .\bin\Debug\net35\ring_of_love.dll "C:\Users\tantu\AppData\Roaming\r2modmanPlus-local\WizardOfLegend\profiles\ring_mod\BepInEx\plugins"
